402dbab67b129dca011293902927c00613baff8f
Dylan Masson <dylanmmasson@gmail.com>
2024-01-12 17:21:24 +0200

    feat: Added keybinds for Neogit and DiffView

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -10,6 +10,8 @@ return {
 		config = function()
 			local neogit = require("neogit")
 			neogit.setup({})
+		vim.keymap.set("n", "<leader>ng", ": Neogit <cr>")
+		vim.keymap.set("n", "<leader>ngc", ": Neogit commit <cr>")
 		end,
 	},
 }
