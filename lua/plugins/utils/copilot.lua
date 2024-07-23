4bde1233b41f7694e48e7bb3ccf7c71223dbbca3
Dylan Masson <dylanmmasson@gmail.com>
2024-07-23 16:57:18 +0200

    Some copilot things

--- lua/plugins/utils/copilot.lua
+++ lua/plugins/utils/copilot.lua
@@ -1,9 +1,10 @@
 return {
-    "github/copilot.vim",
+	"github/copilot.vim",
+	VeryLazy = true,
 
-    config = function()
-        vim.keymap.set("i", "<C-L>", "<Plug>(copilot-accept-word)")
-        vim.keymap.set("i", "<C-]>", "<Plug>(copilot-next)")
-        vim.keymap.set("i", "<C-[>", "<Plug>(copilot-previous)")
-    end,
+	config = function()
+		vim.keymap.set("i", "<C-L>", "<Plug>(copilot-accept-word)")
+		vim.keymap.set("i", "<C-]>", "<Plug>(copilot-next)")
+		vim.keymap.set("i", "<C-[>", "<Plug>(copilot-previous)")
+	end,
 }
