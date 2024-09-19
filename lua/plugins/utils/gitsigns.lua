bd4e6648f5dde204c40f081e28fd583f072312c0
Dylan Masson <dylanmmasson@gmail.com>
2024-09-19 14:50:18 +0200

    Updating gitsigns

--- lua/plugins/utils/gitsigns.lua
+++ lua/plugins/utils/gitsigns.lua
@@ -1,9 +1,10 @@
 return {
 	"lewis6991/gitsigns.nvim",
-	config = function()
-		require("gitsigns").setup()
-		vim.keymap.set("n", "<leader>hp", "<cmd>Gitsigns preview_hunk<CR>")
-		vim.keymap.set("n", "<leader>hb", '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
-		vim.keymap.set("n", "<leader>tb", "<cmd>Gitsigns toggle_current_line_blame<CR>")
-	end,
+
+	opts = {},
+	keys = {
+		{ "<leader>hp", "<cmd>Gitsigns preview_hunk<CR>" },
+		{ "<leader>hb", '<cmd>lua require"gitsigns".blame_line{full=true}<CR>' },
+		{ "<leader>tb", "<cmd>Gitsigns toggle_current_line_blame<CR>" },
+	},
 }
