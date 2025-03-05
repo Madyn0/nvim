cf08b3e4485a830b136c3ac2c81c58c145b06eb6
Dylan Masson <dylanmmasson@gmail.com>
2025-03-05 12:18:11 +0200

    Adding diff view

--- lua/plugins/utils/neogit.lua
+++ lua/plugins/utils/neogit.lua
@@ -1,10 +1,14 @@
 return {
 	"NeogitOrg/neogit",
 
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"sindrets/diffview.nvim",
+	},
 
 	opts = {},
-  event = "VeryLazy",
+	-- event = "VeryLazy",
+	lazy = false,
 
 	keys = {
 		{ "<leader>ng", "<cmd> Neogit <cr>" },
@@ -12,4 +16,14 @@ return {
 		{ "<leader>ngb", "<cmd> Neogit branch <cr>" },
 		{ "<leader>ngp", "<cmd> Neogit push <cr>" },
 	},
+
+	config = function()
+		require("diffview").setup({
+			view = {
+				merge_tool = {
+					layout = "diff3_mixed",
+				},
+			},
+		})
+	end,
 }
