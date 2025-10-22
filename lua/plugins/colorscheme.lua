a92274f9969d655907ae1c2bfd9726c5903c1dec
Dylan Masson <dylanmmasson@gmail.com>
2025-10-22 11:11:12 +0200

    Add vscode colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,37 @@
 return {
-  'projekt0n/github-nvim-theme',
+  {
+    "projekt0n/github-nvim-theme",
 
-  name = 'github-theme',
-  lazy = false,
-  priority = 1000,
+    name = "github-theme",
+    lazy = false,
+    priority = 1000,
 
-  config = function()
-    -- vim.cmd('colorscheme github_dark_high_contrast')
-  end,
+    config = function()
+      -- vim.cmd("colorscheme github_dark_high_contrast")
+    end,
+  },
+  {
+    "Mofiqul/vscode.nvim",
+
+    config = function()
+      require('vscode').setup({
+        -- Alternatively set style in setup
+        style = 'dark',
+
+        -- Enable transparent background
+        transparent = true,
+
+        -- Enable italic comment
+        italic_comments = true,
+
+        -- Enable italic inlay type hints
+        italic_inlayhints = true,
+
+        -- Underline `@markup.link.*` variants
+        underline_links = true,
+      })
+    end,
+    -- load the theme without affecting devicon colors.
+    -- vim.cmd.colorscheme "vscode"
+  }
 }
